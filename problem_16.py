67e3fadf107903384880640632dc4e7b2a08ad35
hlappal <heikki.lappalainen@protonmail.com>
2022-03-24 16:04:13 +0200

    Solved

--- problem_16.py
+++ problem_16.py
@@ -1,13 +1,16 @@
-def get_digits_sum(num):
-    return list(str(num))
+def get_sum(num):
+    digits = list(str(num))
+    sum = 0
+    for d in digits:
+        sum += int(d)
+    return sum
 
 def main():
     pow = 2
 
-    for i in range(1, 15):
+    for i in range(1, 1000):
         pow *= 2
-        print(pow)
 
-    print(get_digits_sum(pow))
+    print(get_sum(pow))
 
 main()
