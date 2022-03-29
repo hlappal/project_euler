5966c34fdc01770fd3a2a6f67e324f59ac274dc0
hlappal <heikki.lappalainen@protonmail.com>
2022-03-29 10:14:52 +0300

    Wrapped util functions to module

--- my_utils.py
+++ my_utils.py
@@ -3,9 +3,8 @@
 from math import sqrt
 
 def is_prime(n):
-    """Determine if a number is prime or not."""
+    """Determine if a number is prime or not"""
     res = True
-
     # Check only numbers larger than 1
     if n < 2:
         res = False
@@ -20,5 +19,25 @@ def is_prime(n):
         for i in range(3, int(sqrt(n))+1, 2):
             if n % i == 0:
                 res = False
-
     return res
+
+def quicksort(array, low, high):
+    """Sort array by Quicksort"""
+    # Find pivot point
+    if low < high:
+        p = quicksort_partition(array, low, high)
+        # Recursive call on the left of pivot
+        quicksort(array, low, p - 1)
+        # Recursive call on the right of pivot
+        quicksort(array, p + 1, high)
+
+def quicksort_partition(array, low, high):
+    """Find partition position for Quicksort"""
+    p = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= p:
+            i += 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
